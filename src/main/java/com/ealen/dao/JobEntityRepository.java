d3def04656ef4c556d1bdec62bd4bde62f22bffd
lihongtao <373309891@qq.com>
2019-06-21 13:28:00 +0800

    增加注解

--- src/main/java/com/ealen/dao/JobEntityRepository.java
+++ src/main/java/com/ealen/dao/JobEntityRepository.java
@@ -2,10 +2,12 @@ package com.ealen.dao;
 
 import com.ealen.entity.JobEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by EalenXie on 2018/6/4 14:27
  */
+@Repository
 public interface JobEntityRepository extends CrudRepository<JobEntity, Long> {
 
     JobEntity getById(Integer id);
